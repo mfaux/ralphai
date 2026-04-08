7974fe0f54893b478663ba4c52bfb90d7128043a
Ralphai <ralphai@users.noreply.github.com>
2026-04-08 13:19:24 +0000

    refactor: consolidate imports and use ensureRepoHasCommit helper

--- src/executor/docker.ts
+++ src/executor/docker.ts
@@ -14,8 +14,7 @@
  */
 
 import { spawn, type ChildProcess } from "child_process";
-import { createWriteStream } from "fs";
-import { existsSync } from "fs";
+import { createWriteStream, existsSync } from "fs";
 import { homedir } from "os";
 import { join } from "path";
 
